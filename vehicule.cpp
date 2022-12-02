109cbb82e12fe9d6c06e6992afe13466ecf8faa3
Frédéric Alphonse <101244166+Fred-23@users.noreply.github.com>
2022-12-02 09:28:45 +0000

    Ajout des différentes méthodes

--- vehicule.cpp
+++ vehicule.cpp
@@ -9,4 +9,50 @@ using namespace std;
 
 #include "vehicule.hpp"
 
-virtual void mettreEnPanne(double random);
+void Vehicule::mettreEnPanne(double random) {
+  if (random<0.5) {
+    this->etat_ = 2;
+  }
+  else {
+    this->etat_ = 3;
+  }
+}
+
+
+void Vehicule::demarrer() {
+  this->vitesse_ = 1;
+}
+
+void Vehicule::arreter() {
+  this->vitesse_ = 0;
+}
+
+void Vehicule::depanner() {
+  this->etat_= 0;
+}
+
+void Vehicule::accelerer(int increment) {
+  this->vitesse_ += 1;
+}
+
+void Vehicule::monter(int nbOcc) {
+  this->occupants_ += nbOcc;
+}
+
+void Vehicule::descendre(int nbOcc) {
+  this->occupants_ -= nbOcc;
+}
+
+string Vehicule::getEtat() {
+  EtatVehicule The_etat;
+  
+  The_etat = EtatVehicule(this->etat_);
+
+  printf("%s",The_etat);
+
+  return The_etat
+  
+}
+
+//Il reste à faire le string::getEtat
+
